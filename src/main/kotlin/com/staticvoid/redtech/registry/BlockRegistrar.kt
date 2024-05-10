108cebbf3d27332aa5fcae31f57b3203bbf03c2d
staticVoidX <81114661+staticVoidX@users.noreply.github.com>
2024-05-10 16:44:01 -0300

    Removed old Kotlin code

--- src/main/kotlin/com/staticvoid/redtech/registry/BlockRegistrar.kt
+++
@@ -1,6 +0,0 @@
-package com.staticvoid.redtech.registry
-
-import net.minecraft.block.Block
-import net.minecraft.registry.Registries
-
-object BlockRegistrar : Registrar<Block>(Registries.BLOCK)
--- src/main/kotlin/com/staticvoid/redtech/registry/ItemRegistrar.kt
+++
@@ -1,6 +0,0 @@
-package com.staticvoid.redtech.registry
-
-import net.minecraft.item.Item
-import net.minecraft.registry.Registries
-
-object ItemRegistrar : Registrar<Item>(Registries.ITEM)
